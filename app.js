0221e3a00ed5c4359e040ea43774febef7d5a6ef
luchobado <luchobadox@gmail.com>
2023-09-06 09:05:37 -0300

    Agregando el Menu + Css + Routing en app.js

--- app.js
+++ app.js
@@ -9,10 +9,33 @@ app.listen(3000, () => console.info('Server arriba'));
 app.use(express.static(path.resolve(__dirname, './src/public')));
 
 //Rutas de acceso a web
-app.get('/', (req,res) => {
+
+/*app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'/src/views/index.html'));
+})*/
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'/src/views/menu.html'));
 })
 
 app.get('/signup', (req,res) => {
     res.sendFile(path.join(__dirname,'/src/views/signup.html'));
 })
+
+app.get('/product-detail', (req,res) => {
+    res.sendFile(path.join(__dirname,'/src/views/detalleDelProducto.html'));
+})
+
+/*  DESCOMENTAR CUANDO ESTE LISTO EL MERGE
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname,'/src/views/login.html'));
+})
+*/
+
+/*  DESCOMENTAR CUANDO ESTE LISTO
+
+app.get('/cart', (req,res) => {
+    res.sendFile(path.join(__dirname,'/src/views/carrito.html'));
+})
+*/
