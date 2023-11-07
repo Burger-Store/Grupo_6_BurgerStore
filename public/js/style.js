e106cc633f9594396f11652d8df7ba4e8b229967
Matias Arevalo <arevalo.matias418@gmail.com>
2023-11-06 21:07:43 -0300

    Logueo, carrito y detalle de producto

--- public/js/style.js
+++ public/js/style.js
@@ -1,13 +1,20 @@
 const menu = document.querySelector('.shop');
+const detalles = document.querySelector('.descripcion')
 const navegation = document.querySelector('.detailsContainer')
 document.addEventListener('DOMContentLoaded',()=>{
     eventos();
+    descripcionHam();
 });
 
 const eventos = () => {
     menu.addEventListener('click', abrirMenu);
+    detalles.addEventListener('click', abrirDetalles);
 }
 
 const abrirMenu = () =>{
+    navegation.classList.remove('hide')
+}
+
+const abrirDetalles = () => {
     navegation.classList.remove('hide')
 }
