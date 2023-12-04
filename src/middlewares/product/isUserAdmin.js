ae98621997ea3b6b55a445ae9d769c897ce29257
Matias Arevalo <arevalo.matias418@gmail.com>
2023-12-04 11:40:03 -0300

    Page no tienes privilegios de administrador

--- src/middlewares/product/isUserAdmin.js
+++ src/middlewares/product/isUserAdmin.js
@@ -2,7 +2,7 @@ const path = require ('path');
 
 function isUserAdmin(req, res, next) {
 	if (!req.session.userLogged || req.session.userLogged.id !== 0) {
-		return res.send("Acceso solo para administradores.");
+		return res.render('accessDanied');
 	}
 	next();
 }
