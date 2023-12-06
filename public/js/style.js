a716de2a4581b6cf67e69630d2387e3222c6fb00
Matias Arevalo <arevalo.matias418@gmail.com>
2023-12-06 15:35:35 -0300

    Open/close details and cart

--- public/js/style.js
+++ public/js/style.js
@@ -1,54 +1,71 @@
-//cart btn
+//boton carrito
 const menu = document.querySelector('.shop');
-//Descripcion del detalle de producto
-//const detalles = document.querySelector('.descripcion')
-//btn x
+//cerrar carrito
+const cerrarCarrito = document.querySelector('.detailsProductHead')
+//Boton hamburguesa
+const openDetailsBurger = document.getElementById('detailBtn');
+//Cerrar detalles de la hamburguesa
+const closeDetailsBurger = document.querySelector('.productEvent');
+
 const iconClose = document.querySelector('.fa-xmark')
 //Contenedor de detalle de producto 
 const navegation = document.querySelector('.detailsContainer')
 //Contenedor de carrito de compras
 const cartNavegation = document.querySelector('.cartContainer');
-//detalle de producto btn
-const detailClose = document.getElementById('detailsClose')
 //Btn "DETALLES" de las hamburgesa
-const actionBtnBurger = document.getElementById('detailBtn')
+const actionBtnBurger = document.getElementById('detailBtn');
+
+
+//Local storage  productEvent
+const cartLocalStorage = window.localStorage;
 
 
 document.addEventListener('DOMContentLoaded',()=>{
     eventos();
     descripcionHam();
-    //CART
-    let cart = JSON.parse(localStorage.getItem('cart') || [])
 });
 
 const eventos = () => {
-    //navbar
-    menu.addEventListener('click', abrirMenu);
-    detailClose.addEventListener('click', closeDetails);
-    detalles.addEventListener('click', abrirDetalles);
-    actionBtnBurger.addEventListener('click',openBurgerDetails);
-    iconClose.addEventListener('click', closeCart)
+    //carrito
+    menu.addEventListener('click', openCart);
+    cerrarCarrito.addEventListener('click', closeCart);
+    //Detalle de la hamburguesa
+    openDetailsBurger.addEventListener('click',openBurgerDetails)
+    closeDetailsBurger.addEventListener('click', closeBurgerDetails)
+
+    //detalles.addEventListener('click', abrirDetalles);
+    //actionBtnBurger.addEventListener('click',openBurgerDetails);
+    //iconClose.addEventListener('click', closeCart)
 
 }
+
+/**
+ * AbrirDetalles --> if isCartOpen ? cerrarCart : null;
+   cerrarDetalles
+
+   abrirCart --> if isDetalleOpen ? cerrarDetalles : null;
+   CerrarCart
+ */
+
+
 //Navbar
-const abrirMenu = () =>{
+const openCart = () =>{
     cartNavegation.classList.remove('hide')
+    navegation.classList.contains('hide') ? '' : navegation.className += ' hide';
 }
-const closeDetails = () => {
-    navegation.className += " hide";
+//Cerrar carrito
+const closeCart = () => {
+    cartNavegation.className += " hide";
 }
-const abrirDetalles = () => {
-    navegation.classList.remove('hide')
-}
-
-const closeCart = () =>{
-    
-    cartNavegation.classList.remove('hide')
-    navegation.className += ' hide';
-}
+//Abrir detalles
 const openBurgerDetails = () => {
+    console.log(openDetailsBurger)
     navegation.classList.remove('hide')
-    cartNavegation.className += ' hide';
+    cartNavegation.classList.contains('hide') ? '' : cartNavegation.className += ' hide';
+}
+//Cerrar detalle
+const closeBurgerDetails = () => {
+    navegation.className += ' hide';
 }
 /**
  * Local storage para carrito
