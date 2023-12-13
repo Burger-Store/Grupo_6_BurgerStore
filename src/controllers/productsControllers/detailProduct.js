0fb3d1f7f81f2e1e1c645cbf21aef8619f7f05bd
Matias Arevalo <arevalo.matias418@gmail.com>
2023-12-13 07:33:19 -0300

    Desestimar el cambio

--- src/controllers/productsControllers/detailProduct.js
+++ src/controllers/productsControllers/detailProduct.js
@@ -4,7 +4,6 @@ const detailsProducts = (req,res) => {
     let burger = db.Products.findByPk(req.params.id)
         
         .then(function(burger){
-            console.log("PASA POR ACAAAAAAAAAAAAAAAAAAAAAAAAAA: " + req.params.id);
             res.render('detalleDelProducto', {burger})
         })
 }
