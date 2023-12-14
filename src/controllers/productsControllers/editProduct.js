7635fb8776b4d838e34a5e95f99b5ef0e49161ff
Matias Arevalo <arevalo.matias418@gmail.com>
2023-12-13 22:15:25 -0300

    Completo el CRUD de producto

--- src/controllers/productsControllers/editProduct.js
+++ src/controllers/productsControllers/editProduct.js
@@ -1,15 +1,16 @@
 const db = require('../../../database/models');
 
 const editProduct = (req, res) => {
+    const product = db.Products.findByPk(req.params.id)
     db.Products.update({
         name: req.body.name,
         description: req.body.description,
         price: req.body.price,
-        category: req.body.category,
-        image: req.file.filename,
+        idcategory: req.body.category,
+        image: req.file ? req.file.filename : product.image
     },{
         where:{
-            id: req.params.id
+            idproducts: req.params.id
         }
     })
     res.redirect(`/update/${req.params.id}`)
