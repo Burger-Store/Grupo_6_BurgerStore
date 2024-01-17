70edc6b1f2c079af70f1aef27e2effd3a6c88d93
Sonyx17 <villafsonia@gmail.com>
2024-01-17 20:43:20 -0300

    Validaciones desde front en login y register

---
+++ public/js/loginValidator.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', function(){
+    let formulario = document.querySelector('form.registration-form');
+    let errores = [];
+
+    formulario.addEventListener('submit', function(e) {
+        
+        let campoEmail = document.querySelector('input.form-control');       
+        if (campoEmail.value == '') {
+            errores.push('El campo email debe estar completo')   
+                   
+        } else if (!campoEmail.value.includes('@')) {
+            errores.push('Debe ingresar un email válido')   
+                     
+        }
+        let campoPassword = document.querySelector('input.form-control');
+
+        if (campoPassword.value == '') {
+            errores.push('Debe ingresar una contraseña')
+            
+        } 
+
+        if (errores.length > 0) {
+            e.preventDefault();
+
+            let ulErrores = document.querySelector('div.errores ul');
+
+            for (let i = 0; i < errores.length; i++) {
+                
+                ulErrores.innerHTML += `<li>${errores[i]}</li>`
+                    
+            }}
+            
+        
+        
+    })
+})
+
+
