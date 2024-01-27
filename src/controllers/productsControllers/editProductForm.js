83b46a738fa1434c39845d89dd70cbab26a20a15
luchobado <luchobadox@gmail.com>
2024-01-27 02:02:53 -0300

    terminando de arreglar la creacion de productos

--- src/controllers/productsControllers/editProductForm.js
+++ src/controllers/productsControllers/editProductForm.js
@@ -4,21 +4,10 @@ const editProductForm = async (req,res) => {
     try {
         const burger = await db.products.findByPk(req.params.id);
         const categoria = await db.category.findAll();
-        res.render('editProduct',{burger, categoria});
+        res.render('editProduct', {burger, categoria});
     } catch (error) {
         res.send(error)
-    }
-    
-    /*db.products.findByPk(req.params.id)
-        .then(function(burger) {
-            return db.category.findAll();
-        })
-        .then(function(categoria) {
-            /*res.render('editProduct', { burger: burger, category: categoria })*/
-            /*res.send({burger, categoria})
-        })*/
-        /*.catch((error) => res.send(error))*/
-    
+    }   
 }
 
 module.exports = editProductForm;
